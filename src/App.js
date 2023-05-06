a6cedc1f7b87243d1a084903e31e6873da3c6042
JM <bakioka@gmail.com>
2023-05-05 22:03:03 -0400

    New change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Changed in feature branch
+        Changed in feature branch 2
         </p>
         <a
           className="App-link"
