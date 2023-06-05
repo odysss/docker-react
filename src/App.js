1250119a1383fd4e9af8afbfd098a943465a9c8c
JM <bakioka@gmail.com>
2023-06-05 16:06:57 -0400

    [Feature-4] Feature number 4 test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Changed in feature branch 3
+        Changed in feature branch 4
         </p>
         <a
           className="App-link"
